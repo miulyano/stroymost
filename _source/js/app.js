43900ea62a9faa0658db0fd652e1510b2afe1032
Максим <dev@Frontend-Maksim.local>
2018-06-17 20:34:57 +0300

    fx style and js

--- _source/js/app.js
+++ _source/js/app.js
@@ -275,3 +275,77 @@ $(function() {
       .on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
   });
 })( jQuery, window, document );
+
+
+// Custom file drop extension
+$.fn.extend({
+  filedrop: function (options) {
+    var defaults = {
+      callback: null
+    };
+    options = $.extend(defaults, options);
+    return this.each(function () {
+      var files = [];
+      var $this = $(this);
+
+      // Stop default browser actions
+      $this.bind('dragover dragleave', function (event) {
+        event.stopPropagation();
+        event.preventDefault()
+      });
+
+      // Catch drop event
+      $this.bind('drop', function (event) {
+        // Stop default browser actions
+        event.stopPropagation();
+        event.preventDefault();
+
+        // Get all files that are dropped
+        files = event.originalEvent.target.files || event.originalEvent.dataTransfer.files;
+
+        // Convert uploaded file to data URL and pass trought callback
+        if (options.callback) {
+          for (i = 0; i < files.length; i++) {
+            var reader = new FileReader();
+            reader.onload = function (event) {
+              options.callback(event.target.result)
+            };
+            reader.readAsDataURL(files[0])
+          }
+        }
+        return false
+      })
+    })
+  }
+});
+
+// Event listener filedropper
+$('#dropZone').filedrop({
+  callback: function (fileEncryptedData) {
+    console.log(fileEncryptedData);
+  }
+});
+
+//выделение цветом зоны #dropZone
+$(document).ready(function($) {
+  $('#dropZone').on({
+    dragenter: function() {
+      $(this).addClass('dropzone-active');
+    },
+    dragleave: function() {
+      $(this).removeClass('dropzone-active');
+    }
+  });
+});
+
+//смещение тултипа с клиентами
+$(function() {
+  $(".customers__block_item").hover(function() {
+    console.log(($(this).find(".customers__tooltip").offset()).left);
+    if(($(this).find(".customers__tooltip").offset()).left > ($(window).width() - $(this).find(".customers__tooltip").outerWidth())) {
+      $(this).find(".customers__tooltip").addClass('right-offset');
+    } else {
+      $(this).find(".customers__tooltip").removeClass('right-offset');
+    }
+  })
+});
